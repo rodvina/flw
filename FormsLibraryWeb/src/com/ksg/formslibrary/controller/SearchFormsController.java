72ed7d53ec4c8d8dfb7465c28b4e6a2673b5bba6
Rodney Odvina <rodvina@kemper.com>
2013-05-02 16:55:49 -0400

    Created ViewFormController

--- FormsLibraryWeb/src/com/ksg/formslibrary/controller/SearchFormsController.java
+++ FormsLibraryWeb/src/com/ksg/formslibrary/controller/SearchFormsController.java
@@ -1,8 +1,11 @@
 package com.ksg.formslibrary.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -16,7 +19,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.ksg.formslibrary.domain.Form;
 import com.ksg.formslibrary.domain.SearchCriteria;
 import com.ksg.formslibrary.service.FormsLibraryService;
-import com.ksg.formslibrary.service.FormsLibraryServiceException;
 
 
 
@@ -28,6 +30,7 @@ import com.ksg.formslibrary.service.FormsLibraryServiceException;
  */
 @Controller("searchFormController")
 @RequestMapping(method=RequestMethod.POST)
+@SessionAttributes(value="searchResults")
 public class SearchFormsController {
 	private static final Logger log = Logger.getLogger(SearchFormsController.class);
 
@@ -36,7 +39,7 @@ public class SearchFormsController {
 	
 	@RequestMapping(value = {"{afl}/internal/search", "{afl}/internal"})
 	public String processSearchInternal(@PathVariable String afl, @ModelAttribute SearchCriteria searchCriteria,
-			BindingResult result, RedirectAttributes redirectAttrs) {
+			BindingResult result, RedirectAttributes redirectAttrs) throws JsonGenerationException, JsonMappingException, IOException {
 		if (result.hasErrors()) {
 			log.error("Errors on search form");
 			return "redirect:/{afl}/internal/search";
@@ -45,8 +48,28 @@ public class SearchFormsController {
 		searchCriteria.setSuppressDuplicates(false);
 		List<Form> searchResults = formsLibraryService.search(afl, searchCriteria);
 		
+		//TODO:  add JSON instead
+//		String results = new ObjectMapper().writeValueAsString(searchResults);
+//		redirectAttrs.addFlashAttribute("results", results);
+//		log.debug("results:"+results);
 		redirectAttrs.addFlashAttribute("searchResults", searchResults);
 		return "redirect:/{afl}/internal/search";
+
+	}
+	
+	@RequestMapping(value = {"{afl}/external/search", "{afl}/external"})
+	public String processSearchExternal(@PathVariable String afl, @ModelAttribute SearchCriteria searchCriteria,
+			BindingResult result, RedirectAttributes redirectAttrs) {
+		if (result.hasErrors()) {
+			log.error("Errors on search form");
+			return "redirect:/{afl}/external/search";
+		}
+		// set to false so dupes will be returned, if any
+		searchCriteria.setSuppressDuplicates(false);
+		List<Form> searchResults = formsLibraryService.search(afl, searchCriteria);
+		
+		redirectAttrs.addFlashAttribute("searchResults", searchResults);
+		return "redirect:/{afl}/external/search";
 
 	}
 	
--- FormsLibraryWeb/src/com/ksg/formslibrary/controller/ViewFormController.java
+++ FormsLibraryWeb/src/com/ksg/formslibrary/controller/ViewFormController.java
@@ -1,12 +1,31 @@
 package com.ksg.formslibrary.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ksg.formslibrary.service.FormsLibraryService;
 
 /**
  * Controller class for viewing a form
  *
  */
-//@Controller
+@Controller
 public class ViewFormController {
+	private static final Logger log = Logger.getLogger(ViewFormController.class);
 
+	@Autowired
+	private FormsLibraryService formsLibraryService;
+	
+	@RequestMapping(value="{afl}/internal/view", method=RequestMethod.GET)
+	public @ResponseBody byte[] processGetDocument(@RequestParam String url) {
+		
+		
+		return null;
+	}
+			
 }
